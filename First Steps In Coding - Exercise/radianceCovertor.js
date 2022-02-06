ff8829d916d65eab84e2b8be6d625809a25b54c7
Denitsa Zhekova <denitsa.zhekova97@gmail.com>
2022-02-06 21:43:59 +0100

    Lecture 2: solutions

---
+++ First Steps In Coding - Exercise/radianceCovertor.js
@@ -0,0 +1,9 @@
+function radiansConvertor(input) {
+    let radians = Number(input[0]);
+    let degree = radians * 180 / Math.PI;
+    degree = degree;
+
+    console.log(degree);
+}
+
+radiansConvertor(['6.2832']);
