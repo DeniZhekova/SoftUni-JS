095c0fb34cf356d42dbbbfb0d0c0636472a98add
Denitsa Zhekova <denitsa.zhekova97@gmail.com>
2022-02-26 13:23:20 +0100

    For Loop - Lab

---
+++ For Loop - Lab/4. even powers of 2.js
@@ -0,0 +1,9 @@
+function evenPowerOf2(input) {
+    let number = Number(input[0]);
+
+    for (let i = 0; i <= number; i++) {
+        if (i % 2 === 0) {
+            console.log(2 ** i);
+        }
+    }
+}
---
+++ For Loop - Lab/7. sum of numbers.js
@@ -0,0 +1,11 @@
+function sumOfNumbers(input) {
+    let numberAsString = input.toString();
+    let sum = 0;
+
+    for (let i = 0; i < numberAsString.length; i++) {
+        let symbol = numberAsString[i];
+        sum += Number(symbol);
+    }
+
+    console.log(`The sum of the digits is:${sum}`);
+}
